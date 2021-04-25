4fc4eb1074d82bdafbfb3e47792e02fe521d3a07
reetah589 <reetah589@gmail.com>
2021-04-24 23:39:08 -0500

    javascript added

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-
-
-// moment().format('MMMM Do YYYY, h:mm:ss a');
+// need to include the date and time
+// moment().format('MMMM Do YYYY, h:mm:ss a');}-
 // moment().format('dddd'); 
+
+var saveButton= $("#save-btn");
+
+    var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+        console.log(currentTime)
+    $("#currentDay").text(currentTime);
+    var currentHour= moment().format('dddd'); 
+
+var workHours =[9,10,11,12,13,14,15,16,17]
+
+var currentMilitaryTime = moment().format('HH');
+
+// schedule work hours blocks
+function displayBlock(){
+    var time = 9;
+    console.log(`comparing ${time} to ${currentMilitaryTime}`)
+    if (time < currentMilitaryTime) {
+        console.log("time is less than currenttime")
+        $(".notes").addClass("past");
+    } else {
+        console.log("time is NOT less than currentTime")
+    }
+}
+
+displayBlock();
+
+
+
